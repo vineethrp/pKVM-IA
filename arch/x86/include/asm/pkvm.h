d2e8fadb99695ffe72e298e0f89c9998653e6742
Vineeth Pillai <vineethrp@gmail.com>
2025-04-09 15:39:12 -0400

    pkvm: delay host iommu driver initialization after deprivilage
    Currently, iommu driver in host is initialized before we deprivilege the
    cpus and this blocks us from paravirtualizing the root and context
    tables. We work around this by adding an ACTIVATE_IOMMU hypercall to get
    the control back of those registers and tables.
    
    Refactor the code to have pkvm initialization code invoke the iommu
    driver initialization in the host. ARM has a similar implementation and
    the basic idea is borrowed from ARM pkvm implementation.
    
    Now that we do not need PKVM_HC_ACTIVATE_IOMMU hypercall, remove it.
    
    iBug: 391542119
    Test: Build and Boot
    
    Change-Id: I2afc655d2d96b4b6cb5c7acce03f1e24dd7b3bcb
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/include/asm/pkvm.h
+++ arch/x86/include/asm/pkvm.h
@@ -15,10 +15,9 @@
 #define PKVM_HC_INIT_FINALISE		1
 #define PKVM_HC_FINALIZE_SHADOW_VM	4
 #define PKVM_HC_MMIO_ACCESS		7
-#define PKVM_HC_ACTIVATE_IOMMU		8
-#define PKVM_HC_TLB_REMOTE_FLUSH_RANGE	9
-#define PKVM_HC_SET_MMIO_VE		10
-#define PKVM_HC_ADD_PTDEV		11
+#define PKVM_HC_TLB_REMOTE_FLUSH_RANGE	8
+#define PKVM_HC_SET_MMIO_VE		9
+#define PKVM_HC_ADD_PTDEV		10
 
 #define PKVM_HC_DUMP_DMAR_TR_STRUCT	20
 #define PKVM_HC_DUMP_DOMAIN_PGT		21
@@ -39,6 +38,14 @@ extern bool __read_mostly enable_pkvm;	/* kernel command-line flag */
 extern struct static_key_false pkvm_ia_enabled_key;
 
 #define pkvm_ia_enabled() (bool)static_branch_likely(&pkvm_ia_enabled_key)
+
+struct pkvm_iommu_driver {
+	int (*prepare_driver)(void);
+	int (*init_driver)(void);
+	void (*remove_driver)(void);
+};
+
+int pkvm_iommu_register_driver(struct pkvm_iommu_driver *kern_ops);
 #endif
 
 DECLARE_PER_CPU_READ_MOSTLY(bool, pkvm_enabled);
