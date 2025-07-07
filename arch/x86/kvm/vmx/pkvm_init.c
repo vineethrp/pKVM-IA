bf680070ea25bfe679964dfa254603bda7287df3
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-07-07 13:58:29 +0800

    pKVM: VMX: Add initial pKVM host support
    Introduce the CONFIG_PKVM_X86 and CONFIG_PKVM_INTEL for enabling the
    pKVM hypervisor support on Intel platform.
    
    The CONFIG_PKVM_X86 is for x86 generic code and the CONFIG_PKVM_INTEL
    is for Intel specific code. Currently the CONFIG_PKVM_X86 cannot be
    enabled standalone. It is selected by the CONFIG_PKVM_INTEL which is the
    only way to enable it at the moment. The reason is that, the entire pkvm
    source code which is going to be rebased doesn't have a clean code split
    between x86 generic and Intel specific. So some x86 generic code may
    still rely on Intel specific code, which can result in x86 generic code
    compiling failed without having Intel specific code.
    
    The pKVM hypervisor enabling on Intel platform will be skipped if the
    enlightened_vmcs is enabled for running with hyperv as the evmcs will
    not be supported.
    
    Without enlightened_evmcs, the vmx driver will try to initialize the pkvm
    driver if the vmx is supported by the hardware. In the initialization, the
    pKVM driver will create a non-root mode environment via the vmx and
    deprivilege the linux kernel which runs as the host VM. The pKVM hypervisor
    code will run in the root mode to handle the vmexits from the host VM.
    
    The current pKVM initialization is a dummy function which will be extended
    by the subsequent patches to implement the detailed logic.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

---
+++ arch/x86/kvm/vmx/pkvm_init.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <asm/cpuid/api.h>
+#include "vmx.h"
+
+int __init vmx_pkvm_init(void)
+{
+	return 0;
+}
+
+MODULE_LICENSE("GPL");
