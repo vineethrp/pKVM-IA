3f7ade71b83544e328a3017f1da8f9d75ebc3ec5
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-14 15:29:47 +0800

    ANDROID: pKVM: x86: Initialize KVM cpuid_xstate_sizes
    Initialize KVM cpuid_xstate_sizes, which will be needed by the pKVM to
    leverage KVM CPUID codes to emulate guest VM's CPUID.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    
    Bug: None
    Change-Id: I7d9028347ceaac82a8b78b05822faf6e866ffa37

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -39,7 +39,6 @@ EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_cpu_caps);
 bool kvm_is_configuring_cpu_caps __read_mostly;
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_is_configuring_cpu_caps);
 
-#ifndef __PKVM_HYP__
 struct cpuid_xstate_sizes {
 	u32 eax;
 	u32 ebx;
@@ -60,6 +59,7 @@ void __init kvm_init_xstate_sizes(void)
 	}
 }
 
+#ifndef __PKVM_HYP__
 u32 xstate_required_size(u64 xstate_bv, bool compacted)
 {
 	u32 ret = XSAVE_HDR_SIZE + XSAVE_HDR_OFFSET;
