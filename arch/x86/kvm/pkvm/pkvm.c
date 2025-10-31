ecb20c3056fa304f640c19ff4d9e0743739ffd31
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-31 16:34:58 +0800

    pKVM: x86: Introduce pkvm_x86_ops
    Introduce pkvm_x86_ops which contains vendor specific operations and is
    used by the pKVM hypervisor only. Currently it contains only two ops:
    - update_vcpu_state_from_host is to update the pKVM hypervisor owned
      vendor specific vCPU state by the data from the host.
    - share_vcpu_state_with_host is to share the pKVM hypervisor owned
      vendor specific vCPU state with the host.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -69,6 +69,11 @@ size_t kvm_vcpu_sz = sizeof(struct kvm_vcpu);
 /* The current loaded guest vCPU. */
 static DEFINE_PER_CPU(struct kvm_vcpu*, cur_guest_vcpu);
 
+static struct pkvm_x86_ops pkvm_x86_ops __read_mostly;
+
+/* TODO: If can be optimized with the static call mechanism. */
+#define pkvm_x86_call(func)		(pkvm_x86_ops.func)
+
 static int __pkvm_vcpu_free(struct pkvm_vm *pkvm_vm, int vcpu_handle,
 			    struct pkvm_memcache *mc);
 
@@ -1370,6 +1375,8 @@ static void update_vcpu_state_from_host(struct kvm_vcpu *vcpu)
 		 */
 		kvm_rip_write(vcpu, shared_vcpu->arch.rip);
 	}
+
+	pkvm_x86_call(update_vcpu_state_from_host)(vcpu);
 }
 
 static void share_vcpu_state_with_host(struct kvm_vcpu *vcpu)
@@ -1418,6 +1425,8 @@ static void share_vcpu_state_with_host(struct kvm_vcpu *vcpu)
 		shared_vcpu->arch.dr6 = vcpu->arch.dr6;
 		shared_vcpu->arch.dr7 = vcpu->arch.dr7;
 	}
+
+	pkvm_x86_call(share_vcpu_state_with_host)(vcpu);
 }
 
 static int pkvm_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit,
@@ -1816,4 +1825,9 @@ unsigned long pkvm_pcpu_tss(int cpu)
 
 	return (unsigned long)&pcpu->tss;
 #endif
+}
+
+void pkvm_x86_ops_init(struct pkvm_x86_ops *ops)
+{
+	memcpy(&pkvm_x86_ops, ops, sizeof(struct pkvm_x86_ops));
 }
