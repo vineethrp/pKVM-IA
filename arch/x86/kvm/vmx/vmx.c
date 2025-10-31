3bae2340a673b17d7168e9f002fd57917e00649f
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-31 15:56:54 +0800

    pKVM: VMX: Sync vCPU state for EXIT_REASON_MSR_WRITE
    Before returning to the host with the EXIT_REASON_MSR_WRITE, the vCPU
    RAX/RDX/RCX and exit instruction length will be shared with the host.
    And Before entering the guest again, skip the wrmsr instruction.
    
    Specifically, all the npVM's vCPU registers are synced unconditionally
    thus only need to share the exit instruction length before returning
    back to the host so that the host can skip the wrmsr instruction. For
    the pVM, no need to share the exit instruction length as the wrmsr
    instruction is skipped by the pKVM hypervisor rather than the host.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -9627,10 +9627,12 @@ static void update_protected_vcpu_state(struct kvm_vcpu *vcpu,
 		break;
 	}
 	case EXIT_REASON_MSR_READ:
+	case EXIT_REASON_MSR_WRITE:
 		if (!pkvm_host_has_emulated_msr(vcpu->kvm, kvm_rcx_read(vcpu)))
 			to_pkvm_vcpu(vcpu)->host_emulated_msr_err = 1;
 
-		if (!to_pkvm_vcpu(vcpu)->host_emulated_msr_err) {
+		if (vmx_get_exit_reason(vcpu).basic == EXIT_REASON_MSR_READ &&
+		    !to_pkvm_vcpu(vcpu)->host_emulated_msr_err) {
 			kvm_rax_write(vcpu, (u32)shared_vcpu->arch.regs[VCPU_REGS_RAX]);
 			kvm_rdx_write(vcpu, (u32)shared_vcpu->arch.regs[VCPU_REGS_RDX]);
 		}
@@ -9692,6 +9694,7 @@ static void share_nonprotected_vcpu_state(struct kvm_vcpu *vcpu,
 	}
 	case EXIT_REASON_IO_INSTRUCTION:
 	case EXIT_REASON_MSR_READ:
+	case EXIT_REASON_MSR_WRITE:
 		/* For the host to skip the instruction for certain exit reasons */
 		shared_vcpu->arch.event_exit_inst_len = vmcs_read32(VM_EXIT_INSTRUCTION_LEN);
 		break;
@@ -9719,6 +9722,10 @@ static void share_protected_vcpu_state(struct kvm_vcpu *vcpu,
 		}
 		break;
 	}
+	case EXIT_REASON_MSR_WRITE:
+		shared_vcpu->arch.regs[VCPU_REGS_RAX] = (u32)kvm_rax_read(vcpu);
+		shared_vcpu->arch.regs[VCPU_REGS_RDX] = (u32)kvm_rdx_read(vcpu);
+		fallthrough;
 	case EXIT_REASON_MSR_READ:
 		shared_vcpu->arch.regs[VCPU_REGS_RCX] = (u32)kvm_rcx_read(vcpu);
 		break;
