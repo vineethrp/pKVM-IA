4d65cb0aadd215383b2c18e91814c53030ea4a33
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-31 14:13:00 +0800

    KVM: pVMX: Implement recalc_intercepts operation
    The MSR interception is controlled by the pKVM hypervisor, there is no
    PV interface exposed for the host to change that. Implementing
    recalc_intercepts with stub code.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -1789,6 +1789,8 @@ static bool pkvm_apic_init_signal_blocked(struct kvm_vcpu *vcpu)
 	return false;
 }
 
+static void pkvm_recalc_intercepts(struct kvm_vcpu *vcpu) {}
+
 static int pkvm_complete_emulated_msr(struct kvm_vcpu *vcpu, int err)
 {
 	if (pkvm_is_protected_vcpu(vcpu)) {
@@ -1933,6 +1935,7 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.check_emulate_instruction = pkvm_check_emulate_instruction,
 	.apic_init_signal_blocked = pkvm_apic_init_signal_blocked,
 
+	.recalc_intercepts = pkvm_recalc_intercepts,
 	.complete_emulated_msr = pkvm_complete_emulated_msr,
 
 	.vcpu_deliver_sipi_vector = kvm_vcpu_deliver_sipi_vector,
