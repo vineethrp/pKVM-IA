7fab62d254f135e77a6ff52a3e5a230f8aadae7c
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 17:56:28 +0800

    pKVM: VMX: Implement enable irq/nmi window operations
    Import vmx_enable_irq/nmi_window() from KVM to implement enabling
    irq/nmi window operations for pKVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -5225,7 +5225,6 @@ void vmx_enable_irq_window(struct kvm_vcpu *vcpu)
 	exec_controls_setbit(to_vmx(vcpu), CPU_BASED_INTR_WINDOW_EXITING);
 }
 
-#ifndef __PKVM_HYP__
 void vmx_enable_nmi_window(struct kvm_vcpu *vcpu)
 {
 	if (!enable_vnmi ||
@@ -5237,6 +5236,7 @@ void vmx_enable_nmi_window(struct kvm_vcpu *vcpu)
 	exec_controls_setbit(to_vmx(vcpu), CPU_BASED_NMI_WINDOW_EXITING);
 }
 
+#ifndef __PKVM_HYP__
 void vmx_inject_irq(struct kvm_vcpu *vcpu, bool reinjected)
 {
 	struct vcpu_vmx *vmx = to_vmx(vcpu);
