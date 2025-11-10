9a7fc6f8271a9dccf418171ee3d06a26a81ea370
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 17:40:25 +0800

    pKVM: VMX: Implement set_efer operation
    Import vmx_set_efer() for the pKVM to implement the set_efer operation.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -1057,6 +1057,7 @@ unsigned int __vmx_vcpu_enter_flags(struct vcpu_vmx *vmx)
 
 	return flags;
 }
+#endif /* !__PKVM_HYP__ */
 
 static __always_inline void clear_atomic_switch_msr_special(struct vcpu_vmx *vmx,
 		unsigned long entry, unsigned long exit)
@@ -1136,7 +1137,17 @@ static void vmx_add_auto_msr(struct vmx_msrs *m, u32 msr, u64 value,
 
 	i = vmx_find_loadstore_msr_slot(m, msr);
 	if (i < 0) {
+		/*
+		 * KVM_BUG_ON() marks the VM dead and broadcasts KVM_REQ_VM_DEAD,
+		 * which is host-only.  pKVM has no remote request mechanism and an
+		 * exhausted fixed autoload array is still an internal invariant
+		 * violation, so report it locally and reject the new entry.
+		 */
+#ifdef __PKVM_HYP__
+		if (WARN_ON_ONCE(m->nr == MAX_NR_LOADSTORE_MSRS))
+#else
 		if (KVM_BUG_ON(m->nr == MAX_NR_LOADSTORE_MSRS, kvm))
+#endif
 			return;
 
 		i = m->nr++;
@@ -1240,6 +1251,7 @@ static bool update_transition_efer(struct vcpu_vmx *vmx)
 	return true;
 }
 
+#ifndef __PKVM_HYP__
 static void vmx_add_autostore_msr(struct vcpu_vmx *vmx, u32 msr)
 {
 	vmx_add_auto_msr(&vmx->msr_autostore, msr, 0, VM_EXIT_MSR_STORE_COUNT,
@@ -2099,7 +2111,6 @@ void vmx_inject_exception(struct kvm_vcpu *vcpu)
 	vmx_clear_hlt(vcpu);
 }
 
-#ifndef __PKVM_HYP__
 static void vmx_setup_uret_msr(struct vcpu_vmx *vmx, unsigned int msr,
 			       bool load_into_hardware)
 {
@@ -2155,6 +2166,7 @@ static void vmx_setup_uret_msrs(struct vcpu_vmx *vmx)
 	vmx->guest_uret_msrs_loaded = false;
 }
 
+#ifndef __PKVM_HYP__
 u64 vmx_get_l2_tsc_offset(struct kvm_vcpu *vcpu)
 {
 	struct vmcs12 *vmcs12 = get_vmcs12(vcpu);
@@ -3479,6 +3491,7 @@ static void enter_rmode(struct kvm_vcpu *vcpu)
 	fix_rmode_seg(VCPU_SREG_GS, &vmx->rmode.segs[VCPU_SREG_GS]);
 	fix_rmode_seg(VCPU_SREG_FS, &vmx->rmode.segs[VCPU_SREG_FS]);
 }
+#endif /* !__PKVM_HYP__ */
 
 int vmx_set_efer(struct kvm_vcpu *vcpu, u64 efer)
 {
@@ -3503,6 +3516,7 @@ int vmx_set_efer(struct kvm_vcpu *vcpu, u64 efer)
 	return 0;
 }
 
+#ifndef __PKVM_HYP__
 #ifdef CONFIG_X86_64
 
 static void enter_lmode(struct kvm_vcpu *vcpu)
