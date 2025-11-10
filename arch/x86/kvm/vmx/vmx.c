682ee354c8a683a41af56f225313a1e1af426890
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 18:02:11 +0800

    pKVM: VMX: Implement sync_pir_to_irr operation
    The vmx_sync_pir_to_irr() is to find out the highest requested interrupt
    from the virtual APIC or posted interrupt descriptor, and sync to guest
    IRR. As the either virtual APIC or posted interrupt are managed by the
    host, the pKVM doesn't response for finding out the highest interrupt
    but response for setting the highest interrupt to the guest IRR. So
    customize vmx_sync_pir_to_irr() as just vmx_set_rvi() for the pKVM.
    
    Introduce a new field in pkvm_vcpu to record the maximum IRR, which will
    be set by the host via the sync_pir_to_irr PV interface in the next
    patch.
    
    [ Chenyi: Introduce max_irr in pkvm_vcpu ]
    
    Co-developed-by: Chenyi Qiang <chenyi.qiang@intel.com>
    Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7254,7 +7254,6 @@ void vmx_hwapic_isr_update(struct kvm_vcpu *vcpu, int max_isr)
 	}
 }
 
-#ifndef __PKVM_HYP__
 static void vmx_set_rvi(int vector)
 {
 	u16 status;
@@ -7274,6 +7273,7 @@ static void vmx_set_rvi(int vector)
 
 int vmx_sync_pir_to_irr(struct kvm_vcpu *vcpu)
 {
+#ifndef __PKVM_HYP__
 	struct vcpu_vt *vt = to_vt(vcpu);
 	int max_irr;
 	bool got_posted_interrupt;
@@ -7314,10 +7314,13 @@ int vmx_sync_pir_to_irr(struct kvm_vcpu *vcpu)
 		vmx_set_rvi(max_irr);
 	else if (got_posted_interrupt)
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
+#else
+	int max_irr = to_pkvm_vcpu(vcpu)->max_irr;
 
+	vmx_set_rvi(max_irr);
+#endif
 	return max_irr;
 }
-#endif /* !__PKVM_HYP__ */
 
 void vmx_load_eoi_exitmap(struct kvm_vcpu *vcpu, u64 *eoi_exit_bitmap)
 {
