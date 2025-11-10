901194d092d834a6995d12f6353aa164d2af99b2
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 18:15:01 +0800

    pKVM: VMX: Implement skip_emulated_instruction operation
    Import vmx_skip_emulated_instruction from KVM to implement the
    skip_emulated_instruction operation for the pKVM hypervisor.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -2064,7 +2064,6 @@ rip_updated:
 	return 1;
 }
 
-#ifndef __PKVM_HYP__
 /*
  * Recognizes a pending MTF VM-exit and records the nested state for later
  * delivery.
@@ -2105,7 +2104,6 @@ int vmx_skip_emulated_instruction(struct kvm_vcpu *vcpu)
 	vmx_update_emulated_instruction(vcpu);
 	return skip_emulated_instruction(vcpu);
 }
-#endif /* !__PKVM_HYP__ */
 
 void vmx_clear_hlt(struct kvm_vcpu *vcpu)
 {
