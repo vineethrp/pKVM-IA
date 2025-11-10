f6569357e77b1c8f3c95f9e0dcd3c716138d68f3
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 17:59:24 +0800

    pKVM: VMX: Implement update_cr8_intercept operations
    Import vmx_update_cr8_intercept() from KVM to implement
    update_cr8_intercept operation for the pKVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7062,6 +7062,7 @@ static noinstr void vmx_l1d_flush(struct kvm_vcpu *vcpu)
 		    [size] "r" (size)
 		: "eax", "ebx", "ecx", "edx");
 }
+#endif /* !__PKVM_HYP__ */
 
 void vmx_update_cr8_intercept(struct kvm_vcpu *vcpu, int tpr, int irr)
 {
@@ -7079,6 +7080,7 @@ void vmx_update_cr8_intercept(struct kvm_vcpu *vcpu, int tpr, int irr)
 		vmcs_write32(TPR_THRESHOLD, tpr_threshold);
 }
 
+#ifndef __PKVM_HYP__
 void vmx_set_virtual_apic_mode(struct kvm_vcpu *vcpu)
 {
 	struct vcpu_vmx *vmx = to_vmx(vcpu);
