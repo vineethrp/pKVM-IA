9c566b96b64f5363cd1528db52e69bc950475790
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 17:45:25 +0800

    pKVM: VMX: Implement cache_reg operation
    Import vmx_cache_reg() from KVM for pKVM to implement cache_reg
    operation.
    
    [ Dmytro: Redefine KVM_BUG_ON for the pKVM hypervisor ]
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -2822,7 +2822,6 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 	return ret;
 }
 
-#ifndef __PKVM_HYP__
 void vmx_cache_reg(struct kvm_vcpu *vcpu, enum kvm_reg reg)
 {
 	unsigned long guest_owned_bits;
@@ -2865,7 +2864,6 @@ void vmx_cache_reg(struct kvm_vcpu *vcpu, enum kvm_reg reg)
 		break;
 	}
 }
-#endif /* !__PKVM_HYP__ */
 
 /*
  * There is no X86_FEATURE for SGX yet, but anyway we need to query CPUID
@@ -3716,6 +3714,7 @@ void vmx_ept_load_pdptrs(struct kvm_vcpu *vcpu)
 		vmcs_write64(GUEST_PDPTR3, vcpu->arch.pdptrs[3]);
 	}
 }
+#endif /* !__PKVM_HYP__ */
 
 void ept_save_pdptrs(struct kvm_vcpu *vcpu)
 {
@@ -3730,6 +3729,7 @@ void ept_save_pdptrs(struct kvm_vcpu *vcpu)
 	kvm_register_mark_available(vcpu, VCPU_REG_PDPTR);
 }
 
+#ifndef __PKVM_HYP__
 #define CR3_EXITING_BITS (CPU_BASED_CR3_LOAD_EXITING | \
 			  CPU_BASED_CR3_STORE_EXITING)
 
