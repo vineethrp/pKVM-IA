cf284336e1a5cb7c706ab5dc056937b74bbe6bda
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 18:11:56 +0800

    pKVM: VMX: Implement handle_exit_irqoff operation
    Import vmx_handle_exit_irqoff from KVM to implement handle_exit_irqoff
    operation for the pKVM hypervisor. Although the irq is always off in the
    pKVM, still implement this to reuse the vmexit handlers in this
    operation. The detailed handlers for the EXIT_REASON_EXTERNAL_INTERRUPT
    and the EXIT_REASON_EXCEPTION_NMI will be added later.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7742,12 +7742,14 @@ static void handle_external_interrupt_irqoff(struct kvm_vcpu *vcpu,
 
 	vcpu->arch.at_instruction_boundary = true;
 }
+#endif /* !__PKVM_HYP__ */
 
 void vmx_handle_exit_irqoff(struct kvm_vcpu *vcpu)
 {
 	if (to_vt(vcpu)->emulation_required)
 		return;
 
+#ifndef __PKVM_HYP__
 	switch (vmx_get_exit_reason(vcpu).basic) {
 	case EXIT_REASON_EXTERNAL_INTERRUPT:
 		handle_external_interrupt_irqoff(vcpu, vmx_get_intr_info(vcpu));
@@ -7761,8 +7763,12 @@ void vmx_handle_exit_irqoff(struct kvm_vcpu *vcpu)
 	default:
 		break;
 	}
+#else
+	/* TODO */
+#endif
 }
 
+#ifndef __PKVM_HYP__
 /*
  * The kvm parameter can be NULL (module initialization, or invocation before
  * VM creation). Be sure to check the kvm parameter before using it.
