d59fa726bb32c90486369ebec63b90f0ab73a4bb
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 18:01:06 +0800

    pKVM: VMX: Implement load_eoi_exitmap operation
    Import vmx_load_eoi_exitbimap() from KVM to implement load_eio_bitmap
    operations for pKVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7574,6 +7574,7 @@ int vmx_sync_pir_to_irr(struct kvm_vcpu *vcpu)
 
 	return max_irr;
 }
+#endif /* !__PKVM_HYP__ */
 
 void vmx_load_eoi_exitmap(struct kvm_vcpu *vcpu, u64 *eoi_exit_bitmap)
 {
@@ -7586,6 +7587,7 @@ void vmx_load_eoi_exitmap(struct kvm_vcpu *vcpu, u64 *eoi_exit_bitmap)
 	vmcs_write64(EOI_EXIT_BITMAP3, eoi_exit_bitmap[3]);
 }
 
+#ifndef __PKVM_HYP__
 static void handle_nm_fault_irqoff(struct kvm_vcpu *vcpu)
 {
 	/*
