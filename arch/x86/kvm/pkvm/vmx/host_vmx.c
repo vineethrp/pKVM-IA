7bd5bfc5e14ea7a4b45fe607e3eb0977bc6213ac
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 17:55:41 +0800

    pKVM: VMX: Implement interrupt shadow operations
    Import vmx_set/get_interrupt_shadow() from KVM to implement the
    interrupt shadow operations for pKVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/vmx/host_vmx.c
+++ arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -36,22 +36,6 @@ static struct pkvm_init_ops vmx_init_ops = {
 
 struct pkvm_init_ops *pkvm_vmx_init_ops = &vmx_init_ops;
 
-void vmx_set_interrupt_shadow(struct kvm_vcpu *vcpu, int mask)
-{
-	u32 interruptibility_old = vmcs_read32(GUEST_INTERRUPTIBILITY_INFO);
-	u32 interruptibility = interruptibility_old;
-
-	interruptibility &= ~(GUEST_INTR_STATE_STI | GUEST_INTR_STATE_MOV_SS);
-
-	if (mask & KVM_X86_SHADOW_INT_MOV_SS)
-		interruptibility |= GUEST_INTR_STATE_MOV_SS;
-	else if (mask & KVM_X86_SHADOW_INT_STI)
-		interruptibility |= GUEST_INTR_STATE_STI;
-
-	if (interruptibility != interruptibility_old)
-		vmcs_write32(GUEST_INTERRUPTIBILITY_INFO, interruptibility);
-}
-
 static void skip_emulated_instruction(struct kvm_vcpu *vcpu)
 {
 	unsigned long rip;
