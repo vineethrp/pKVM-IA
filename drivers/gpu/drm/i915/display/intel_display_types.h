6047bac45ab73ff1ee3aa6a04aaa0643b4e6c7a0
Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
2025-12-03 14:21:58 +0530

    UPSTREAM: drm/i915: Add intel_color_op
    Add data structure to store intel specific details of colorop
    
    v2:
     - Remove dead code
     - Convert macro to function (Jani)
     - Remove colorop state as it is not being used
     - Refactor to separate file
    
    Reviewed-by: Suraj Kandpal <suraj.kandpal@intel.com>
    Signed-off-by: Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Link: https://patch.msgid.link/20251203085211.3663374-3-uma.shankar@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    (cherry picked from commit 3e9b06559aa1c3daa21ff34a25b103f3fa7f0c7a)
    
    Upstream-Task: 480179178
    Bug: 438112500
    Bug: 480179178
    Test: HDR support
    Change-Id: I697beecdccf5baa87d4d3c8d489308b2efffee6d
    Signed-off-by: Ap, Kamal <kamal.ap@intel.corp-partner.google.com>

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -1936,6 +1936,11 @@ struct intel_dp_mst_encoder {
 	struct intel_connector *connector;
 };
 
+struct intel_colorop {
+	struct drm_colorop base;
+	enum intel_color_block id;
+};
+
 static inline struct intel_encoder *
 intel_attached_encoder(struct intel_connector *connector)
 {
