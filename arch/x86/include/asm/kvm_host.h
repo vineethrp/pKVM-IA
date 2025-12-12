5724524bd57fd54c216fde5dafe8595e2471eb79
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-12-12 13:13:02 +0800

    pKVM: x86: trace: Register per guest VM vmexit trace debug file
    Register a per guest VM debug file pkvm_vmexit_trace, which can be used
    to show the trace of this single guest VM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -2094,6 +2094,7 @@ extern phys_addr_t pkvm_mem_base;
 extern phys_addr_t pkvm_mem_size;
 void __init pkvm_reserve(void);
 void pkvm_init_debugfs(void);
+void pkvm_create_vm_debugfs(struct kvm *kvm);
 void kvm_free_pkvm_memcache(struct pkvm_memcache *mc);
 
 DECLARE_STATIC_KEY_FALSE(pkvm_enabled_key);
@@ -2107,6 +2108,7 @@ static inline bool pkvm_enabled(void)
 static inline bool pkvm_enabled(void) { return false; }
 
 static inline void __init pkvm_reserve(void) {}
+static inline void pkvm_create_vm_debugfs(struct kvm *kvm) {}
 #endif
 
 #ifdef __PKVM_HYP__
