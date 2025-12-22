ae07b6a80e7e64ab8efb7d46ece8f7b16cb80263
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-12-22 16:48:27 +0800

    pKVM: x86: Add cache_reg PV interface
    Add cache_reg PV interface to allow the host to get some of the guest
    registers via the vendor cache_reg operation. This PV interface is not
    allowed for the host to cache registers for a pVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -466,6 +466,8 @@ static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate 
 		vcpu->arch.perf_capabilities = kvm_caps.supported_perf_cap;
 	}
 
+	vcpu->arch.walk_mmu = &vcpu->arch.root_mmu;
+
 	ret = kvm_x86_call(vcpu_create)(vcpu);
 	if (ret)
 		goto unsetup_lapic;
@@ -793,6 +795,52 @@ static int pkvm_set_msr(struct kvm_vcpu *vcpu, u32 index, u64 data)
 	return kvm_msr_write(vcpu, index, data);
 }
 
+static int pkvm_cache_reg(struct kvm_vcpu *vcpu, enum kvm_reg reg,
+			  union pkvm_hc_data *out)
+{
+	switch (reg) {
+	case VCPU_REGS_RSP:
+	case VCPU_REG_RIP:
+	case VCPU_REG_PDPTR:
+	case VCPU_REG_CR0:
+	case VCPU_REG_CR3:
+	case VCPU_REG_CR4:
+		break;
+	default:
+		return -EOPNOTSUPP;
+	}
+
+	kvm_x86_call(cache_reg)(vcpu, reg);
+
+	switch (reg) {
+	case VCPU_REGS_RSP:
+		out->cache_reg.rsp = vcpu->arch.regs[VCPU_REGS_RSP];
+		break;
+	case VCPU_REG_RIP:
+		out->cache_reg.rip = vcpu->arch.rip;
+		break;
+	case VCPU_REG_PDPTR:
+		out->cache_reg.pdptrs[0] = vcpu->arch.pdptrs[0];
+		out->cache_reg.pdptrs[1] = vcpu->arch.pdptrs[1];
+		out->cache_reg.pdptrs[2] = vcpu->arch.pdptrs[2];
+		out->cache_reg.pdptrs[3] = vcpu->arch.pdptrs[3];
+		break;
+	case VCPU_REG_CR0:
+		out->cache_reg.cr0 = vcpu->arch.cr0;
+		break;
+	case VCPU_REG_CR3:
+		out->cache_reg.cr3 = vcpu->arch.cr3;
+		break;
+	case VCPU_REG_CR4:
+		out->cache_reg.cr4 = vcpu->arch.cr4;
+		break;
+	default:
+		BUG();
+	}
+
+	return 0;
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *out)
 {
@@ -822,6 +870,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__get_msr:
 		ret = kvm_msr_read(vcpu, pkvm_hc_input1(hvcpu), &out->get_msr.data);
+		break;
+	case __pkvm__cache_reg:
+		ret = pkvm_cache_reg(vcpu, pkvm_hc_input1(hvcpu), out);
 		break;
 	default:
 		ret = -EINVAL;
