bbdcaa42d56b1b30cf8ab66110f38fa3215c3f27
Dmytro Maluka <dmaluka@google.com>
2026-01-02 21:22:51 +0000

    pKVM: VMX: Implement get_mt_mask operation
    Import vmx_get_mt_mask() from KVM to reuse it for getting the desired
    values of memory type bits for guest MMU PTEs for the given VM.
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -14349,6 +14349,7 @@ void kvm_arch_unregister_noncoherent_dma(struct kvm *kvm)
 	if (!atomic_dec_return(&kvm->arch.noncoherent_dma_count))
 		kvm_noncoherent_dma_assignment_start_or_stop(kvm);
 }
+#endif /* !__PKVM_HYP__ */
 
 bool kvm_arch_has_noncoherent_dma(struct kvm *kvm)
 {
@@ -14356,6 +14357,7 @@ bool kvm_arch_has_noncoherent_dma(struct kvm *kvm)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_arch_has_noncoherent_dma);
 
+#ifndef __PKVM_HYP__
 bool kvm_arch_no_poll(struct kvm_vcpu *vcpu)
 {
 	return (vcpu->arch.msr_kvm_poll_control & 1) == 0;
