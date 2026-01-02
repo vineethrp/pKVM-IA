34bd0a7760b331a16f711133690ccf911f542cd9
Dmytro Maluka <dmaluka@google.com>
2026-01-02 21:22:51 +0000

    pKVM: VMX: Implement get_mt_mask operation
    Import vmx_get_mt_mask() from KVM to reuse it for getting the desired
    values of memory type bits for guest MMU PTEs for the given VM.
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -999,9 +999,11 @@ struct kvm_x86_ops vt_x86_ops __initdata = {
 
 	.set_tss_addr = vt_op(set_tss_addr),
 	.set_identity_map_addr = vt_op(set_identity_map_addr),
+#endif
 	.get_mt_mask = vmx_get_mt_mask,
 	.tdp_has_smep = vt_op(tdp_has_smep),
 
+#ifndef __PKVM_HYP__
 	.get_exit_info = vt_op(get_exit_info),
 	.get_entry_info = vt_op(get_entry_info),
 #endif
