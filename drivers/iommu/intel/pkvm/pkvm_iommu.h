92213d142a788b89eb228f0caf21436bac745833
Vineeth Pillai <vineethrp@google.com>
2026-01-08 09:14:45 -0500

    pkvm: iommu/vt-d: Hypercall wrappers for dmar_read/write
    Use iommu_mmio_read/write hypercalls to access IOMMU registers when
    pkvm is enabled.
    
    Change-Id: I9a3e499d575a578cc06d2f2f5f994cac598bbf13
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/pkvm/pkvm_iommu.h
+++ drivers/iommu/intel/pkvm/pkvm_iommu.h
@@ -35,6 +35,48 @@ PKVM_DECLARE(int, prepare_iommu, (struct intel_iommu_info *info));
 
 #ifndef __PKVM_HYP__
 
+static inline u64 pkvm_readq(void __iomem *reg, unsigned long reg_phys, unsigned long offset)
+{
+	union pkvm_hc_data d;
+
+	if (pkvm_enabled())
+		pkvm_hypercall_out(iommu_mmio_read, &d, reg_phys + offset, sizeof(u64));
+	else
+		d.iommu_mmio_read.val = readq(reg + offset);
+
+	return d.iommu_mmio_read.val;
+}
+
+static inline u32 pkvm_readl(void __iomem *reg, unsigned long reg_phys, unsigned long offset)
+{
+	union pkvm_hc_data d;
+
+	if (pkvm_enabled())
+		pkvm_hypercall_out(iommu_mmio_read, &d, reg_phys + offset, sizeof(u32));
+	else
+		d.iommu_mmio_read.val = readl(reg + offset);
+
+	return d.iommu_mmio_read.val;
+}
+
+static inline void pkvm_writeq(void __iomem *reg, unsigned long reg_phys,
+			       unsigned long offset, u64 val)
+{
+	if (pkvm_enabled())
+		pkvm_hypercall(iommu_mmio_write, reg_phys + offset, sizeof(u64), val);
+	else
+		writeq(val, reg + offset);
+}
+
+static inline void pkvm_writel(void __iomem *reg, unsigned long reg_phys,
+			       unsigned long offset, u32 val)
+{
+	if (pkvm_enabled())
+		pkvm_hypercall(iommu_mmio_write, reg_phys + offset, sizeof(u32), val);
+	else
+		writel(val, reg + offset);
+}
+
 int __init pkvm_host_prepare_iommu(void);
 int __init pkvm_host_init_iommu(void);
 #else /* __PKVM_HYP__ */
