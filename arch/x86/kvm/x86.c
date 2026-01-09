1a058ef0c6d4f5c21ed09cb8f2c82e1c6d0e9ddb
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-09 14:28:13 +0800

    ANDROID: pKVM: VMX: Handle host EPT violation
    Although all the memory information is mapped during the initialize, as
    well as the MMIO holes, the MMIO in the high-end address is not mapped
    and accessing such MMIO from the host VM will trigger EPT violation.
    Meanwhile, accessing an isolated memory from the host VM can also
    trigger the EPT violation.
    
    To handle this vmexit, the GPA should be checked to see if it is not
    a memory address or not. The EPT violation caused by accessing memory is
    considered as a security violation, and the pKVM hypervisor will inject
    GP exception to the host. Only the EPT violation caused by accessing the
    MMIO will be handled by install a mapping in the host ept with the
    possible maximum page size, to minimize the memory consumptions. As this
    would happen on multiple CPU at the same time, introduce the host_mmu
    spinlock to avoid concurrency.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    
    Bug: None
    Change-Id: I87a82cf667eee843412183c30dc13a91efb7eb92

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -830,7 +830,6 @@ void kvm_deliver_exception_payload(struct kvm_vcpu *vcpu,
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_deliver_exception_payload);
 
-#ifndef __PKVM_HYP__
 static void kvm_queue_exception_vmexit(struct kvm_vcpu *vcpu, unsigned int vector,
 				       bool has_error_code, u32 error_code,
 				       bool has_payload, unsigned long payload)
@@ -845,7 +844,6 @@ static void kvm_queue_exception_vmexit(struct kvm_vcpu *vcpu, unsigned int vecto
 	ex->has_payload = has_payload;
 	ex->payload = payload;
 }
-#endif /* !__PKVM_HYP__ */
 
 static void kvm_multiple_exception(struct kvm_vcpu *vcpu, unsigned int nr,
 				   bool has_error, u32 error_code,
@@ -924,12 +922,15 @@ void kvm_queue_exception_p(struct kvm_vcpu *vcpu, unsigned nr,
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_queue_exception_p);
 
+#endif /* !__PKVM_HYP__ */
+
 static void kvm_queue_exception_e_p(struct kvm_vcpu *vcpu, unsigned nr,
 				    u32 error_code, unsigned long payload)
 {
 	kvm_multiple_exception(vcpu, nr, true, error_code, true, payload);
 }
 
+#ifndef __PKVM_HYP__
 void kvm_requeue_exception(struct kvm_vcpu *vcpu, unsigned int nr,
 			   bool has_error_code, u32 error_code)
 {
@@ -981,6 +982,8 @@ static int complete_emulated_insn_gp(struct kvm_vcpu *vcpu, int err)
 				       EMULTYPE_COMPLETE_USER_EXIT);
 }
 
+#endif /* !__PKVM_HYP__ */
+
 void kvm_inject_page_fault(struct kvm_vcpu *vcpu, struct x86_exception *fault)
 {
 	++vcpu->stat.pf_guest;
@@ -998,6 +1001,7 @@ void kvm_inject_page_fault(struct kvm_vcpu *vcpu, struct x86_exception *fault)
 					fault->address);
 }
 
+#ifndef __PKVM_HYP__
 void kvm_inject_emulated_page_fault(struct kvm_vcpu *vcpu,
 				    struct x86_exception *fault)
 {
