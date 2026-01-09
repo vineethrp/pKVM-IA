cc345234bc28025424cf6c585a7f27b642e93d5e
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-09 12:21:57 +0800

    pKVM: x86: Facilitate the hypervisor to use pkvm_hyp components
    The pKVM hypervisor may also needs to access the pkvm_hyp at the running
    time, as well the host vcpu and physical cpu. Define a global pkvm_hyp
    pointer to point to the pkvm_hyp memory. Define percpu kvm_vcpu pointer
    for the host vcpu and percpu pkvm_pcpu pointer for the physical cpu
    respectively. These are initialized before deprivilege the host.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/types.h>
+#include "pkvm.h"
 
 /*
  * Needed by kvm_spurious_fault(), which is a generic fault handler for
@@ -8,3 +9,7 @@
  * the hypervisor image and virt_rebooting is always false.
  */
 __visible bool virt_rebooting;
+
+struct pkvm_hyp *pkvm_hyp;
+DEFINE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
+DEFINE_PER_CPU(struct kvm_vcpu *, host_vcpu);
