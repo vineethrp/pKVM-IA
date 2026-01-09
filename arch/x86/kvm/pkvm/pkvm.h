9ee8dcf94f3474de19daaa14cf931f1d7510a6a1
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-09 16:03:34 +0800

    pKVM: x86: Add pkvm_x86_vendor_init()
    Similar to the KVM, add pkvm_x86_vendor_init() for the vendor code to
    initialize the basic infrastructures needed for running guest VMs.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.h
+++ arch/x86/kvm/pkvm/pkvm.h
@@ -2,6 +2,7 @@
 #ifndef __PKVM_X86_PKVM_H
 #define __PKVM_X86_PKVM_H
 
+#include <linux/kvm_host.h>
 #include <asm/kvm_pkvm.h>
 
 DECLARE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
@@ -9,5 +10,6 @@ DECLARE_PER_CPU(struct kvm_vcpu *, host_vcpu);
 
 void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu);
 void pkvm_kick_vcpu(struct kvm_vcpu *vcpu);
+int pkvm_x86_vendor_init(struct kvm_x86_init_ops *ops);
 
 #endif /* __PKVM_X86_PKVM_H */
