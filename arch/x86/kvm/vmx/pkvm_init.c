3e199aa9827cd009ef3efed3ad36b07e81e85310
Vineeth Pillai <vineethrp@google.com>
2026-01-09 12:37:12 -0500

    iommu/vt-d: pKVM: Delay IOMMU initialization to after pKVM init
    Delay the IOMMU initialization after pKVM init so that pKVM can trap the
    IOMMU register accesses and setup IOMMU without going through the whole
    translation structures.
    
    The initialization is in 3 phases.
    1. Preparation - Host validates the IOMMU compatibility with pKVM and
       sets up pKVM IOMMU data structures before deprivilege.
    2. Init - pKVM performs basic initialization for all IOMMUs in the
       system.
    3. Activation - pKVM traps SRTP and sets up root table pointer and
       isolated IOMMU registers from host. SRTP happens during early boot
       during host iommu_init callback and host is considered trusted. All
       IOMMU operations from here on goes through pKVM and pKVM can enforce
       security policies.
    
    This patch implements the delayed initialization framework with stubs for
    preparation(1) and init(2).
    
    Since pKVM depends on INTEL_IOMMU from this patch, add the dependency on
    KConfig explicitly.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm_init.c
+++ arch/x86/kvm/vmx/pkvm_init.c
@@ -10,6 +10,7 @@
 #include <asm/set_memory.h>
 #include "pkvm_constants.h"
 #include "vmx.h"
+#include "pkvm_iommu.h"
 
 extern u64 x86_pred_cmd;
 
@@ -1495,6 +1496,10 @@ int __init vmx_pkvm_init(void)
 		pr_cont("reboot with kvm-intel.pkvm_relax_cpu_bugs=false\n");
 	}
 
+	ret = pkvm_host_prepare_iommu();
+	if (ret)
+		goto out;
+
 	pkvm_sym(init_ops) = pkvm_sym(pkvm_vmx_init_ops);
 
 	pkvm_ramoops_init();
@@ -1507,6 +1512,12 @@ int __init vmx_pkvm_init(void)
 	if (ret)
 		goto repriv_cpus;
 	static_branch_enable(&pkvm_enabled_key);
+
+	ret = pkvm_host_init_iommu();
+	if (ret) {
+		static_branch_disable(&pkvm_enabled_key);
+		goto repriv_cpus;
+	}
 
 	/*
 	 * After host deprivileging succeed, un-present the kernel direct
@@ -1544,6 +1555,9 @@ out:
 	 * can clear it while we are still in VMX non-root.
 	 */
 	pkvm_firmware_rmem_clear();
+
+	/* TODO: Try re-initialize IOMMU */
+
 	/*
 	 * As the reserved memory at the pkvm_mem_base will not be
 	 * released back to the host, no need to de-initialize or
