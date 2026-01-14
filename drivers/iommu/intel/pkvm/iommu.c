e68d9f291b676a45d3c5a6128ceccf11a3daadbd
Vineeth Pillai <vineethrp@google.com>
2026-01-14 10:38:34 -0500

    pkvm: iommu/vt-d: Handle DMA_GCMD_TE
    Trap TE bit and enable translation on behalf of the host. For a disable
    translation request, pKVM doesn't really disable translation, as it
    allows devices to bypass pKVM security guarantees in accessing memory.
    So GSTS is marked to reflect the disable request so that host sees the
    translation disabled when reading GSTS.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/pkvm/iommu.c
+++ drivers/iommu/intel/pkvm/iommu.c
@@ -23,7 +23,7 @@ unsigned int iommu_pglvl_mask = IOMMU_PGT_4LEVEL | IOMMU_PGT_5LEVEL;
 /* GCMD oneshot bits where unsetting the bit doesn't have an effect */
 #define DMAR_GCMD_ONESHOT	(DMA_GCMD_SRTP | DMA_GCMD_SIRTP)
 /* Mask of bits the host is allowed to access directly (passed through to hardware) */
-#define DMAR_GCMD_DIRECT	(DMA_GCMD_TE | DMA_GCMD_IRE | DMA_GCMD_CFI | DMA_GCMD_SIRTP)
+#define DMAR_GCMD_DIRECT	(DMA_GCMD_IRE | DMA_GCMD_CFI | DMA_GCMD_SIRTP)
 /* Mask of bits supported by pKVM */
 #define DMAR_GCMD_SUPPORTED_BITS	(DMAR_GSTS_EN_BITS | DMA_GCMD_SRTP | DMA_GCMD_SIRTP)
 
@@ -210,6 +210,30 @@ static void handle_gcmd_srtp(struct intel_iommu *iommu)
 	return;
 }
 
+static void handle_gcmd_te(struct intel_iommu *iommu, bool enable)
+{
+	if (enable) {
+		if (iommu->vgsts & DMA_GSTS_TES) {
+			pkvm_err("iommu%d: TE allowed only once\n", iommu->seq_id);
+			return;
+		} else if (!(iommu->vgsts & DMA_GSTS_RTPS)) {
+			pkvm_err("iommu%d: TE not allowed before SRTP\n", iommu->seq_id);
+			return;
+		}
+
+		handle_gcmd_direct(iommu, DMA_GCMD_TE, true);
+		iommu->vgsts |= DMA_GSTS_TES;
+		pkvm_dbg("iommu%d: Translation enabled!\n", iommu->seq_id);
+	} else {
+		/*
+		 * Translation is not really disabled as it would
+		 * compromise pKVM security guarantees.
+		 */
+		iommu->vgsts &= ~DMA_GSTS_TES;
+		pkvm_dbg("iommu%d: Translation marked as disabled!", iommu->seq_id);
+	}
+}
+
 static void handle_global_cmd(struct intel_iommu *iommu, u32 val)
 {
 	u32 changed = (iommu->vgsts & DMAR_GSTS_EN_BITS) ^ val;
@@ -239,6 +263,11 @@ static void handle_global_cmd(struct intel_iommu *iommu, u32 val)
 
 	if (changed & DMA_GCMD_SRTP) {
 		handle_gcmd_srtp(iommu);
+		return;
+	}
+
+	if (changed & DMA_GCMD_TE) {
+		handle_gcmd_te(iommu, !!(val & changed));
 		return;
 	}
 
