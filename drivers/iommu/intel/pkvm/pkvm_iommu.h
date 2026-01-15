a844263135a0d7ecf0a9cfb920eabe453c2f2e04
Vineeth Pillai <vineethrp@google.com>
2026-01-15 06:44:14 -0500

    pkvm: iommu/vt-d: IOMMU initialization during pkvm init
    Implement the preparation and initialization logic for pKVM IOMMU. Host
    does few basic validation to see if IOMMU is usable by pKVM and then
    initializes hypervisor IOMMU datastructures before deprivilege. This is
    followed by pKVM hypervisor initialization which calls into IOMMU init
    hook and maps the IOMMU MMIO space.
    
    Change-Id: I41967c43bf4d1ca08952c4db84fd56f63ab9658e
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/pkvm/pkvm_iommu.h
+++ drivers/iommu/intel/pkvm/pkvm_iommu.h
@@ -6,11 +6,33 @@
 #ifndef _PKVM_INTEL_IOMMU_H_
 #define _PKVM_INTEL_IOMMU_H_
 
+/* expose pkvm_enabled() when !CONFIG_PKVM_X86 */
+#include <asm/kvm_host.h>
+#include <asm/kvm_pkvm.h>
+
+struct intel_iommu_info {
+	u64 reg_phys;
+	u64 reg_size;
+	u64 cap;
+	u64 ecap;
+	int seq_id;
+	int agaw;
+	int msagaw;
+	u16 segment;
+};
+
+#ifdef CONFIG_PKVM_INTEL
+extern int pkvm_sym(intel_iommu_sm);
+
+PKVM_DECLARE(int, prepare_iommu, (struct intel_iommu_info *info));
+
 #ifndef __PKVM_HYP__
+
 int __init pkvm_host_prepare_iommu(void);
 int __init pkvm_host_init_iommu(void);
-#else
-int pkvm_intel_iommu_init(void);
-#endif
+#else /* __PKVM_HYP__ */
 
-#endif
+int pkvm_intel_iommu_init(void);
+#endif /* !__PKVM_HYP__ */
+#endif /* CONFIG_PKVM_INTEL */
+#endif /* _PKVM_INTEL_IOMMU_H_ */
