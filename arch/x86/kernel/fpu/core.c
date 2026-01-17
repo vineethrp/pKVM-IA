9bab110c3e1c4008e21624adcc0a39cd5695f347
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-17 08:03:17 +0800

    pKVM: x86: Initialize the percpu fpu
    Initialize the percpu current_task fpu with the initial fpstate, which is
    used to restore the FPU to the initial state when switches from pVMs to
    the host VM, so that to isolation pVM's FPU from host. The permission is
    set to the maximum features to allow the guest to use all the supported
    features.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kernel/fpu/core.c
+++ arch/x86/kernel/fpu/core.c
@@ -534,6 +534,7 @@ static inline unsigned int init_fpstate_copy_size(void)
 	/* XSAVE(S) just needs the legacy and the xstate header part */
 	return sizeof(init_fpstate.regs.xsave);
 }
+#endif /* !__PKVM_HYP__ */
 
 static inline void fpstate_init_fxstate(struct fpstate *fpstate)
 {
@@ -572,6 +573,7 @@ void fpstate_init_user(struct fpstate *fpstate)
 		fpstate_init_fstate(fpstate);
 }
 
+#ifndef __PKVM_HYP__
 static void __fpstate_reset(struct fpstate *fpstate)
 {
 	/*
