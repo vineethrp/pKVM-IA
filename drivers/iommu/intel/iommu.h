7c426a3cfddd1d468bd510c949f036961060e476
Vineeth Pillai <vineethrp@google.com>
2026-01-17 12:18:53 -0500

    pkvm: iommu/vt-d: Port legacy mode context table helpers to pKVM
    Port functionality to clear and set legacy mode context table entries
    to pKVM.
    
    This would be used by pkVM LM context update hypercalls.
    
    Change-Id: If26a4a673049ddc6d8210c60ee386b5bab37aec6
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/iommu.h
+++ drivers/iommu/intel/iommu.h
@@ -28,6 +28,7 @@
 #include <asm/iommu.h>
 #include <uapi/linux/iommufd.h>
 
+#include <asm/pkvm_spinlock.h>
 #include "pkvm/pkvm_iommu.h"
 
 /*
@@ -836,6 +837,13 @@ struct intel_iommu {
 	struct q_inval  *qi;    /* Pointer to _qi. Enables host code re-use */
 	struct iommu_flush flush;
 	struct root_entry *root_entry;
+	/*
+	 * Virtual address of page donated by host for constructing
+	 * translation structures(context table/pasid table). This
+	 * page is passed in by hypercalls that construct the
+	 * translation structures.
+	 */
+	void		*ts_page;
 	pkvm_spinlock_t lock;
 };
 
@@ -884,6 +892,10 @@ struct dev_pasid_info {
 struct device_domain_info {
 	u8 bus;			/* PCI bus number */
 	u8 devfn;		/* PCI devfn number */
+	u16 pfsid;		/* SRIOV physical function source ID */
+	u8 ats_supported:1;
+	u8 ats_enabled:1;
+	u8 ats_qdep;
 	struct intel_iommu *iommu; /* IOMMU used by this device */
 };
 #endif /* !__PKVM_HYP__ */
@@ -1113,6 +1125,16 @@ static inline void context_clear_entry(struct context_entry *context)
 	context->lo = 0;
 	context->hi = 0;
 }
+
+void domain_context_clear_one(struct device_domain_info *info,
+			      u8 bus, u8 devfn);
+int domain_context_mapping_one(struct dmar_domain *domain,
+			       struct intel_iommu *iommu,
+#ifdef __PKVM_HYP__
+			       struct device_domain_info *info,
+			       u16 did,
+#endif
+			       u8 bus, u8 devfn);
 
 #ifndef __PKVM_HYP__
 #ifdef CONFIG_INTEL_IOMMU
