2b821022ada651194c7b0215abe305a6969c3a69
Vineeth Pillai <vineethrp@google.com>
2026-01-18 09:59:34 -0500

    pkvm: x86: spinlock api wrappers for iommu locking
    Implement spinlock lock/unlock wrappers which could be used for pKVM
    and host to help host driver code sharing with pKVM. The wrappers
    internally call into respective implementation of spinlock apis.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/iommu.h
+++ drivers/iommu/intel/iommu.h
@@ -816,6 +816,9 @@ struct intel_iommu {
 
 	struct iommu_pmu *pmu;
 };
+
+#define iommu_spin_lock(iommu)		spin_lock(&(iommu)->lock);
+#define iommu_spin_unlock(iommu)	spin_unlock(&(iommu)->lock);
 #else
 struct intel_iommu {
 	void __iomem	*reg; /* Pointer to hardware regs, virtual addr */
@@ -835,6 +838,9 @@ struct intel_iommu {
 	struct root_entry *root_entry;
 	pkvm_spinlock_t lock;
 };
+
+#define iommu_spin_lock(iommu)		pkvm_spin_lock(&(iommu)->lock);
+#define iommu_spin_unlock(iommu)	pkvm_spin_unlock(&(iommu)->lock);
 #endif /* !__PKVM_HYP__ */
 
 #ifndef __PKVM_HYP__
