8989c98e1b9bc9dd2144c0f1fbd9b830df6340a3
Vineeth Pillai <vineethrp@google.com>
2026-01-24 21:24:32 -0500

    pkvm: iommu/vt-d: iommu_flush callbacks for pKVM
    Host driver has a set of callbacks for IOMMU flush logic and gets
    dynamically set based on QI or register based invalidation. Now that
    pKVM has QI helper functions ported, we can reuse this flush callback
    helpers for implementing flushing logic.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/pkvm/iommu.c
+++ drivers/iommu/intel/pkvm/iommu.c
@@ -115,6 +115,9 @@ static void initialize_qi(struct intel_iommu *iommu)
 	}
 	*/
 
+	iommu->flush.flush_context = qi_flush_context;
+	iommu->flush.flush_iotlb = qi_flush_iotlb;
+
 	pkvm_spin_lock_init(&qi->q_lock);
 	qi->free_head = qi->free_tail = 0;
 	qi->free_cnt = QI_LENGTH;
