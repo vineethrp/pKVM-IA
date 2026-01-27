dd3a23ecac8d240aa115cf4c7d7e405393d3b714
Vineeth Pillai <vineethrp@google.com>
2026-01-27 12:16:03 -0500

    pkvm: iommu/vt-d: Scalable mode context update hypercalls
    Implement hypercalls for setting and clearing scalable mode context
    table entries and host driver changes to use the hypercall.
    
    Change-Id: Ib3acbe184271ebfa116a7440458f3a85fd6002c8
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/pkvm/iommu_hc.h
+++ drivers/iommu/intel/pkvm/iommu_hc.h
@@ -12,6 +12,7 @@ enum iommu_hc_num {
 	qi_submit,
 	clear_ce,
 	set_lm_ce,
+	set_sm_ce
 };
 
 struct qi_submit_data {
@@ -43,11 +44,26 @@ struct set_lm_ce_data {
 	u8 ats_supported: 1;
 };
 
+struct set_sm_ce_data {
+	u64 phys;
+	u64 pasid_table_gpa;
+	u64 ts_page_gpa;
+	u32 max_pasid;
+	u8 bus;
+	u8 devfn;
+	u8 ats_qdep;
+	u8 ats_enabled: 1;
+	u8 ats_supported: 1;
+	u8 pasid_supported: 3;
+	u8 pasid_enabled: 1;
+};
+
 struct iommu_hc_data {
 	union {
 		struct qi_submit_data qi_submit;
 		struct clear_ce_data clear_ce;
 		struct set_lm_ce_data set_lm_ce;
+		struct set_sm_ce_data set_sm_ce;
 	};
 	u8 hc_num;
 };
@@ -56,4 +72,5 @@ static_assert(sizeof(struct iommu_hc_data) <= PKVM_HC_DATA_MAX_NUM * sizeof(u64)
 int pkvm_iommu_qi_submit(struct qi_submit_data *data);
 int pkvm_iommu_clear_ce(struct clear_ce_data *data);
 int pkvm_iommu_set_lm_ce(struct set_lm_ce_data *data);
+int pkvm_iommu_set_sm_ce(struct set_sm_ce_data *data);
 #endif /* _PKVM_INTEL_IOMMU_HC_H_ */
