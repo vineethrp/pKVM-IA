f2e983f0f43e76e8afe3d59002120420fe8b6ed6
Aashish Sharma <shraash@google.com>
2026-01-28 09:53:30 -0500

    ANDROID: iommu/vt-d: pKVM: Port domain page table handling code to pKVM
    To minimize code duplication, this change reuses significant portions of
    the Intel IOMMU host driver logic (specifically the page table walk and
    entry management) by compiling the relevant files with the __PKVM_HYP__
    flag. This allows the hypervisor to leverage established functions like
    pfn_to_dma_pte() and domain_map() while substituting host-specific
    primitives with pKVM-compliant ones.
    
    Bug: 473885291
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: Ib368e992cf3d46756a5f05a45f998c1433482e7c
    Signed-off-by: Aashish Sharma <shraash@google.com>

--- drivers/iommu/intel/iommu.h
+++ drivers/iommu/intel/iommu.h
@@ -738,6 +738,7 @@ struct dmar_domain {
 
 	atomic_t refcount;
 	unsigned int index;
+	struct pkvm_memcache mc;
 	/*
 	 * Lock to protect the mapping operations
 	 * on this domain.
@@ -1077,6 +1078,14 @@ static inline int width_to_agaw(int width)
 {
 	return DIV_ROUND_UP(width - 30, LEVEL_STRIDE);
 }
+
+int domain_map(struct dmar_domain *domain, unsigned long iov_pfn,
+		 unsigned long phys_pfn, unsigned long nr_pages, int prot,
+		 gfp_t gfp);
+
+void domain_unmap(struct dmar_domain *domain, unsigned long start_pfn,
+			 unsigned long last_pfn,
+			 struct iommu_pages_list *freelist);
 
 static inline unsigned int level_to_offset_bits(int level)
 {
