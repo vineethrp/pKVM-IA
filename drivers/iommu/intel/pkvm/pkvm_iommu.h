246c3d318f80eff6982f03961ffe18165f736f53
Vineeth Pillai <vineethrp@google.com>
2026-01-28 21:31:23 -0500

    pkvm: iommu/vt-d: Pasid table hypercall for first level translation
    Implement hypercalls for setting pasid table first level translation
    details and host driver changes to use the hypercall.
    
    Change-Id: I63bddca09129a8aa8d6f93fecabf3e40b3a0053a
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/pkvm/pkvm_iommu.h
+++ drivers/iommu/intel/pkvm/pkvm_iommu.h
@@ -101,6 +101,8 @@ int pv_context_mapping(struct intel_iommu *iommu, struct device_domain_info *inf
 		       u8 bus, u8 devfn, u64 pgd_gpa, u16 did, u8 agaw);
 int pv_pasid_table_setup(struct intel_iommu *iommu, struct device_domain_info *info,
 			 u8 bus, u8 devfn);
+int pv_pasid_setup_fl(struct device_domain_info *info, phys_addr_t fsptptr,
+		      u32 pasid, u16 did, u16 old_did, int flags);
 #else /* __PKVM_HYP__ */
 
 bool is_dev_in_satc(u16 bdf);
