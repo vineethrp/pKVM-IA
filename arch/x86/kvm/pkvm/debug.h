a23fa155c192ed66c5324ba3a08d7e050830da37
Vineeth Pillai <vineethrp@google.com>
2026-02-23 10:17:59 -0500

    DONOTMERGE: Enable pkvm debug
    Change-Id: Ic52acb310e33564a34af3b7d2d492026227f8daf

--- arch/x86/kvm/pkvm/debug.h
+++ arch/x86/kvm/pkvm/debug.h
@@ -11,7 +11,7 @@
 #define pkvm_info(f, x...)		pr_info("pkvm [CPU%d]: " f, raw_smp_processor_id(), ## x)
 
 #ifdef CONFIG_PKVM_X86_DEBUG
-#define pkvm_dbg(f, x...)		pr_debug("pkvm [CPU%d]: " f, raw_smp_processor_id(), ## x)
+#define pkvm_dbg(f, x...)		pr_info("pkvm [CPU%d]: " f, raw_smp_processor_id(), ## x)
 #else
 #define pkvm_dbg(f, x...)		no_printk(KERN_DEBUG pr_fmt(f), ## x)
 #endif
