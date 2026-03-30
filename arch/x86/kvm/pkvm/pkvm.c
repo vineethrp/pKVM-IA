6212559575835d2822455c27499d72f186dde0ea
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-03-30 10:50:28 +0800

    pKVM: x86: Add guest-vCPU iterator and synchronize guest EPT flush
    Introduce for_each_pkvm_guest_vcpu(i, vcpu, vm) to iterate over all
    non-NULL vCPUs of a guest VM. This replaces the for loops in the
    pkvm_vm_finalize(), pkvm_start_secondary_vcpu(), guest_ept_flush_tlb(),
    and the trace helpers.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    
    Use the iterator and wait helper after guest-EPT invalidation so no remote guest vCPU can retain access through a stale translation.

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -368,6 +368,7 @@ static int pkvm_vm_finalize(int vm_handle)
 {
 	struct kvm *kvm, *shared_kvm;
 	struct pkvm_vm *pkvm_vm;
+	struct kvm_vcpu *vcpu;
 	gpa_t pvmfw_load_addr;
 	int ret = 0, i;
 
@@ -403,9 +404,7 @@ static int pkvm_vm_finalize(int vm_handle)
 		kvm->arch.pkvm.pvmfw_load_addr = pvmfw_load_addr;
 	}
 
-	for (i = 0; i < kvm->created_vcpus; i++) {
-		struct kvm_vcpu *vcpu = &pkvm_vm->vcpus[i]->vcpu;
-
+	for_each_pkvm_guest_vcpu(i, vcpu, pkvm_vm) {
 		if (vcpu->vcpu_id == kvm->arch.bsp_vcpu_id) {
 			/*
 			 * Make sure pvmfw_load_addr and bsp_vcpu_id are updated before
@@ -2276,6 +2275,7 @@ unsigned long pkvm_pcpu_tss(int cpu)
 int pkvm_start_secondary_vcpu(struct kvm *kvm, u32 apic_id, unsigned long start_ip)
 {
 	struct pkvm_vm *pkvm_vm = to_pkvm(kvm);
+	struct kvm_vcpu *vcpu;
 	int ret = -EINVAL;
 	int i;
 
@@ -2287,9 +2287,7 @@ int pkvm_start_secondary_vcpu(struct kvm *kvm, u32 apic_id, unsigned long start_
 
 	pkvm_spin_lock(&pkvm_vm->lock);
 
-	for (i = 0; i < kvm->created_vcpus; i++) {
-		struct kvm_vcpu *vcpu = &pkvm_vm->vcpus[i]->vcpu;
-
+	for_each_pkvm_guest_vcpu(i, vcpu, pkvm_vm) {
 		if (vcpu->vcpu_id != apic_id)
 			continue;
 
