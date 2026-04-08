2cbb3e824ad71268b5f571d8e0f0dec6518c3b20
Grzegorz Jaszczyk <jaszczyk@google.com>
2026-04-08 22:00:20 +0000

    pKVM: x86: Dynamically discover ramoops from platform bus
    To support autonomous ramoops-based logging, the hypervisor needs to
    know the physical base address and size of the persistent RAM buffer.
    
    Retrieve the physical base address of the ramoops region dynamically
    from the ACPI bus (GOOG9999).
    
    Target the console buffer within the ramoops region. On GOOG9999 pstore,
    the memory is partitioned as:
    [Dmesg Area] [Console (128KB)] [Pmsg (128KB)] [Ftrace (128KB)]
    
    Since ramoops must recover logs across hardware resets, potentially
    across different kernel version, this partitioning scheme is expected to
    remain stable (chromeos_pstore.c's chromeos_ramoops_data hasn't changed
    for a decade).
    
    Targeting the console buffer allows the hypervisor to append its panic
    log to the existing host console log. This way the crash record doesn't
    overwrite the host's final ramoops-dmesg records nor the existing
    ramoops-console (to which it only appends).
    
    This is possible because console-ramoops relies on the
    'persistent_ram_buffer' structure, which has a 'start' field that acts
    as a write pointer. This allows the hypervisor to easily identify
    where the host stopped logging. In contrast, dmesg-ramoops doesn't rely
    on persistent_ram_buffer and it is more complex to append hyp
    bookkeeping.
    
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -32,6 +32,10 @@ struct pkvm_hyp *pkvm_hyp;
 DEFINE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
 DEFINE_PER_CPU(struct kvm_vcpu *, host_vcpu);
 DEFINE_PER_CPU(bool, host_vcpu_fixup);
+
+phys_addr_t pkvm_ramoops_console_pa;
+size_t pkvm_ramoops_console_size;
+
 /*
  * similarly pmu.c is not compiled. define kvm_mmu_cap here for the use
  * in cpuid.c
--- arch/x86/kvm/vmx/pkvm_init.c
+++ arch/x86/kvm/vmx/pkvm_init.c
@@ -1480,6 +1480,8 @@ int __init vmx_pkvm_init(void)
 
 	pkvm_sym(init_ops) = pkvm_sym(pkvm_vmx_init_ops);
 
+	pkvm_ramoops_init();
+
 	ret = pkvm_host_deprivilege_cpus(pkvm);
 	if (ret)
 		goto repriv_cpus;
