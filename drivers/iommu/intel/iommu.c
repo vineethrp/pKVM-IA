71dd0a61fdaf72e3fc9f5f21a7631579191aa26e
Vineeth Pillai <vineethrp@google.com>
2026-05-27 09:21:46 -0400

    iommu/vt-d: pKVM: WARN_ON iommu suspend/resume on S3
    pKVM doesn't support S3 suspend. Return EOPNOTSUPP on the suspend
    callback.
    
    WARN_ON() on both suspend and resume callbacks as well.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -2693,6 +2693,10 @@ static int iommu_suspend(void)
 	struct intel_iommu *iommu = NULL;
 	unsigned long flag;
 
+	/* pKVM doesn't support S3 suspend */
+	if (WARN_ON(pkvm_enabled()))
+		return -EOPNOTSUPP;
+
 	iommu_flush_all();
 
 	for_each_active_iommu(iommu, drhd) {
@@ -2719,6 +2723,10 @@ static void iommu_resume(void)
 	struct dmar_drhd_unit *drhd;
 	struct intel_iommu *iommu = NULL;
 	unsigned long flag;
+
+	/* pKVM doesn't support S3 suspend */
+	if (WARN_ON(pkvm_enabled()))
+		return;
 
 	if (init_iommu_hw()) {
 		if (force_on)
