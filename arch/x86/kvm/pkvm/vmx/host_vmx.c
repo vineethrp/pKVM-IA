cxdong/pub-pKVM-IA

DEBUG: pKVM: x86: Print error logs in some error code path
Use pkvm_err to print error logs in some error code path for an easy
debugging.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -3,6 +3,7 @@
 #include <linux/memblock.h>
 #include <kvm_emulate.h>
 #include <vmx/x86_ops.h>
+#include "debug.h"
 #include "ept.h"
 #include "host_vmx.h"
 #include "init_finalize.h"
@@ -250,6 +251,8 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 		skip_instruction = true;
 		break;
 	default:
+		pkvm_err_ratelimited("Unsupported vmexit reason 0x%x.\n",
+				      vmx->exit_reason.full);
 		break;
 	}
 

vineethrp/pKVM-IA

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -3,6 +3,7 @@
 #include <linux/memblock.h>
 #include <kvm_emulate.h>
 #include <vmx/x86_ops.h>
+#include "debug.h"
 #include "ept.h"
 #include "host_vmx.h"
 #include "pkvm/init.h"
@@ -346,6 +347,8 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 			skip_instruction = true;
 		break;
 	default:
+		pkvm_err_ratelimited("Unsupported vmexit reason 0x%x.\n",
+				      vt->exit_reason.full);
 		break;
 	}
 