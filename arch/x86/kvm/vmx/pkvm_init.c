cxdong/pub-pKVM-IA

pKVM: x86: Add host vcpu instance
Add instances to represent the host vCPU. Each phys CPU will have a host
vcpu instance. On Intel processors, the vcpu instance is actually
pointing to the vcpu_vmx structure, thus reserve the corresponding
memory pages according to the vcpu_vmx structure size.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_init.c
@@ -50,6 +50,28 @@ static __init int pkvm_setup_pcpu(struct pkvm_hyp *pkvm, int cpu)
 	return 0;
 }
 
+static __init int pkvm_setup_host_vcpu(struct pkvm_hyp *pkvm, int cpu)
+{
+	struct vcpu_vmx *vmx;
+
+	if (cpu >= CONFIG_NR_CPUS) {
+		pr_err("setup_host_vcpu: invalid CPU number %d\n", cpu);
+		return -EINVAL;
+	}
+
+	vmx = pkvm_early_alloc_contig(PKVM_HOST_VCPU_VMX_PAGES);
+	if (!vmx) {
+		pr_err("no host vcpu memory for CPU%d\n", cpu);
+		return -ENOMEM;
+	}
+
+	vmx->vcpu.cpu = cpu;
+	vmx->vcpu.kvm = pkvm->host_kvm;
+	pkvm->host_vcpus[cpu] = &vmx->vcpu;
+
+	return 0;
+}
+
 int __init vmx_pkvm_init(void)
 {
 	unsigned long nr_pages;
@@ -83,6 +105,9 @@ int __init vmx_pkvm_init(void)
 
 	for_each_possible_cpu(cpu) {
 		ret = pkvm_setup_pcpu(pkvm, cpu);
+		if (ret)
+			goto out;
+		ret = pkvm_setup_host_vcpu(pkvm, cpu);
 		if (ret)
 			goto out;
 	}

vineethrp/pKVM-IA

arch/x86/kvm/vmx/pkvm_init.c
@@ -12,6 +12,7 @@ static int __init early_pkvm_parse_cmdline(char *buf)
 early_param("kvm-intel.pkvm", early_pkvm_parse_cmdline);
 
 static DEFINE_PER_CPU(struct pkvm_pcpu*, pkvm_pcpu);
+static DEFINE_PER_CPU(struct kvm_vcpu*, host_vcpu);
 
 u64 pkvm_total_reserve_pages(void)
 {
@@ -53,6 +54,28 @@ static __init int pkvm_setup_pcpu(int cpu)
 	return 0;
 }
 
+static __init int pkvm_setup_host_vcpu(struct kvm *kvm, int cpu)
+{
+	struct vcpu_vmx *vmx;
+
+	if (cpu >= CONFIG_NR_CPUS) {
+		pr_err("setup_host_vcpu: invalid CPU number %d\n", cpu);
+		return -EINVAL;
+	}
+
+	vmx = pkvm_early_alloc_contig(PKVM_HOST_VCPU_VMX_PAGES);
+	if (!vmx) {
+		pr_err("no host vcpu memory for CPU%d\n", cpu);
+		return -ENOMEM;
+	}
+
+	vmx->vcpu.cpu = cpu;
+	vmx->vcpu.kvm = kvm;
+	per_cpu(host_vcpu, cpu) = &vmx->vcpu;
+
+	return 0;
+}
+
 int __init vmx_pkvm_init(void)
 {
 	unsigned long nr_pages;
@@ -89,7 +112,12 @@ int __init vmx_pkvm_init(void)
 		if (ret)
 			goto out;
 
+		ret = pkvm_setup_host_vcpu(pkvm->host_kvm, cpu);
+		if (ret)
+			goto out;
+
 		pkvm->pcpus[pkvm->num_cpus] = per_cpu(pkvm_pcpu, cpu);
+		pkvm->host_vcpus[pkvm->num_cpus] = per_cpu(host_vcpu, cpu);
 		pkvm->num_cpus++;
 	}
 