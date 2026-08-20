cxdong/pub-pKVM-IA

pKVM: x86: Add hypervisor panic and reset infrastructure
The pKVM hypervisor should be capable of handling fatal internal errors
(BUGs or hardware exceptions) without relying on host kernel code or
state. Letting the hypervisor jump back into the host panic path would
allow an untrusted host to inspect an unpredictable state of a buggy
hypervisor.

Introduce a standalone hypervisor panic and recovery subsystem:

1. Redefine BUG() and BUG_ON() within the hypervisor to trigger
   'pkvm_panic()', ensuring all fatal assertions are handled
   locally.
2. Implement 'pkvm_panic()':
   - Uses a global 'pkvm_panic_in_progress' atomic flag to coordinate
     the shootdown across all cores.
   - Broadcasts an INIT to all other cores via the x2APIC to pull
     them out of the host VM.
   - VM exit handler catch and hold all CPUs in a halt loop when panic
     is in-progress, ensuring the host VM cannot interfere with the
     panic bookkeeping.
3. Implement 'pkvm_emergency_reset()' to ensure system recovery
   regardless of host state. It attempts a multi-stage hardware reset:
   - PCI System Soft Reset (0xCF9/0x06)
   - PS/2 Keyboard Controller reset (0x64/0xFE).
   - PCI System Hard Reset (0xCF9/0x0E).
   - Triple Fault (Invalid IDT + int3) as a final fail-safe.
4. Redefine BUG() to pass __FILE__ and __LINE__ as direct arguments,
   consciously avoiding the use of 'ud2' instructions and the
   associated 'bug_table' binary metadata. This is important for
   production (non-debug) environments where the hypervisor doesn't
   have its own kallsyms.

This commit provides the infrastructure for hypervisor bug reporting,
which will be extended in subsequent patches to support persistent
ramoops logging.

Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/pkvm/panic.c
@@ -1,0 +1,91 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/kernel.h>
+#include <asm/io.h>
+#include <asm/apic.h>
+#include <asm/kvm_pkvm.h>
+#include <asm/msr.h>
+#include <asm-generic/bug.h>
+#include "debug.h"
+#include "pkvm.h"
+#include "memory.h"
+
+/*
+ * To not include ACPI based reboot and its complexity, try to reset the system
+ * via PCI warm reset (0xCF9), the keyboard controller (PS/2), PCI cold reset
+ * register (CF9), or triple fault.
+ *
+ * All based on arch/x86/kernel/reboot.c native_machine_emergency_restart().
+ */
+static void __noreturn pkvm_emergency_reset(void)
+{
+	struct desc_ptr idt;
+
+	/*
+	 * PCI warm reset (0xCF9), which is usually effectively the ACPI reset,
+	 * i.e. the modern recommended default.
+	 */
+	outb(0x06, 0xcf9);
+	pkvm_udelay(50000);
+
+	/* Keyboard controller (PS/2) reset */
+	outb(0xfe, 0x64);
+	pkvm_udelay(50000);
+
+	/* PCI cold reset */
+	outb(0x0e, 0xcf9);
+	pkvm_udelay(50000);
+
+	/* Triple fault */
+	idt.size = 0;
+	idt.address = 0;
+	asm volatile("lidt %0" : : "m"(idt));
+	asm volatile("int3");
+
+	while (1)
+		asm volatile("cli; hlt");
+}
+
+atomic_t pkvm_panic_in_progress = ATOMIC_INIT(0);
+
+void __noreturn pkvm_panic(const char *fmt, ...)
+{
+	static char panic_msg[1024];
+	va_list args;
+
+	/*
+	 * Ensure only one CPU handles the panic and writes to ramoops.
+	 * This also signals the VM exit handler to catch and hold all other
+	 * CPUs.
+	 */
+	if (atomic_cmpxchg(&pkvm_panic_in_progress, 0, 1) != 0) {
+		while (1)
+			asm volatile("cli; hlt");
+	}
+
+	/*
+	 * Broadcast INIT to all other CPUs (excluding self) via x2APIC ICR
+	 * to pull them out of the guest mode. This ensures that the host will not
+	 * interfere with panic handling and e.g. will not interfere with
+	 * ramoops update.
+	 */
+	native_x2apic_icr_write(APIC_DEST_ALLBUT | APIC_INT_ASSERT | APIC_DM_INIT, 0);
+
+	/*
+	 * Wait for all IPIs to deliver and targets to enter Wait-for-SIPI.
+	 * Note: using pkvm_wait_vcpu_kicked_out() is tempting but for reliability,
+	 * since we are in the middle of fatal error handling, it is better to
+	 * use simple hw delay. This avoids relying on various sw states (like
+	 * vcpu->mode) that could be corrupted by the fatal error itself.
+	 */
+	pkvm_udelay(10000);
+
+	va_start(args, fmt);
+	vscnprintf(panic_msg, sizeof(panic_msg), fmt, args);
+	va_end(args);
+
+	pkvm_err("%s", panic_msg);
+
+	/* TODO: add ramoops logging */
+
+	pkvm_emergency_reset();
+}

vineethrp/pKVM-IA

arch/x86/kvm/pkvm/panic.c
@@ -1,0 +1,91 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/kernel.h>
+#include <asm/io.h>
+#include <asm/apic.h>
+#include <asm/kvm_pkvm.h>
+#include <asm/msr.h>
+#include <asm-generic/bug.h>
+#include "debug.h"
+#include "pkvm.h"
+#include "memory.h"
+
+/*
+ * To not include ACPI based reboot and its complexity, try to reset the system
+ * via PCI warm reset (0xCF9), the keyboard controller (PS/2), PCI cold reset
+ * register (CF9), or triple fault.
+ *
+ * All based on arch/x86/kernel/reboot.c native_machine_emergency_restart().
+ */
+static void __noreturn pkvm_emergency_reset(void)
+{
+	struct desc_ptr idt;
+
+	/*
+	 * PCI warm reset (0xCF9), which is usually effectively the ACPI reset,
+	 * i.e. the modern recommended default.
+	 */
+	outb(0x06, 0xcf9);
+	pkvm_udelay(50000);
+
+	/* Keyboard controller (PS/2) reset */
+	outb(0xfe, 0x64);
+	pkvm_udelay(50000);
+
+	/* PCI cold reset */
+	outb(0x0e, 0xcf9);
+	pkvm_udelay(50000);
+
+	/* Triple fault */
+	idt.size = 0;
+	idt.address = 0;
+	asm volatile("lidt %0" : : "m"(idt));
+	asm volatile("int3");
+
+	while (1)
+		asm volatile("cli; hlt");
+}
+
+atomic_t pkvm_panic_in_progress = ATOMIC_INIT(0);
+
+void __noreturn panic(const char *fmt, ...)
+{
+	static char panic_msg[1024];
+	va_list args;
+
+	/*
+	 * Ensure only one CPU handles the panic and writes to ramoops.
+	 * This also signals the VM exit handler to catch and hold all other
+	 * CPUs.
+	 */
+	if (atomic_cmpxchg(&pkvm_panic_in_progress, 0, 1) != 0) {
+		while (1)
+			asm volatile("cli; hlt");
+	}
+
+	/*
+	 * Broadcast INIT to all other CPUs (excluding self) via x2APIC ICR
+	 * to pull them out of the guest mode. This ensures that the host will not
+	 * interfere with panic handling and e.g. will not interfere with
+	 * ramoops update.
+	 */
+	native_x2apic_icr_write(APIC_DEST_ALLBUT | APIC_INT_ASSERT | APIC_DM_INIT, 0);
+
+	/*
+	 * Wait for all IPIs to deliver and targets to enter Wait-for-SIPI.
+	 * Note: using pkvm_wait_vcpu_kicked_out() is tempting but for reliability,
+	 * since we are in the middle of fatal error handling, it is better to
+	 * use simple hw delay. This avoids relying on various sw states (like
+	 * vcpu->mode) that could be corrupted by the fatal error itself.
+	 */
+	pkvm_udelay(10000);
+
+	va_start(args, fmt);
+	vscnprintf(panic_msg, sizeof(panic_msg), fmt, args);
+	va_end(args);
+
+	pkvm_err("%s", panic_msg);
+
+	/* TODO: add ramoops logging */
+
+	pkvm_emergency_reset();
+}