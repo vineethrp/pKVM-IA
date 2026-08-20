cxdong/pub-pKVM-IA

pKVM: x86: trace: Register vmexit_trace debugfs
Register read-only vmexit_trace debugfs in /sys/kernel/debug/pkvm/.
Reading this debugfs will make the host use dump_vmexit_trace PV
interface to get the vmexit tracing data from the pKVM hypervisor and
print to the stdout.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm_debugfs.c
@@ -4,6 +4,7 @@
 #include <linux/debugfs.h>
 #include <linux/kvm_host.h>
 #include <asm/kvm_pkvm.h>
+#include <asm/pkvm_trace.h>
 
 static void enable_vmexit_trace_func(void *data)
 {
@@ -27,6 +28,125 @@ static int enable_vmexit_trace(void *data, u64 val)
 }
 DEFINE_SIMPLE_ATTRIBUTE(enable_vmexit_trace_fops, NULL, enable_vmexit_trace, "%llu\n");
 
+static const struct vmexit_reason {
+	int reason;
+	char *name;
+} reasons[] = { VMX_EXIT_REASONS, { -1, NULL } };
+
+static const char *get_vmexit_reason(int reason)
+{
+	const struct vmexit_reason *p = reasons;
+
+	while (p->name) {
+		if (p->reason == reason)
+			return p->name;
+		p++;
+	}
+
+	return NULL;
+}
+
+static int print_perf_data(struct seq_file *m, struct perf_data *summary,
+			   struct perf_data *pervcpu)
+{
+	struct perf_data *print = pervcpu ? pervcpu : summary;
+	struct vmexit_stats *stats, *summary_stats;
+	const char *prefix;
+	int *vcpu_id;
+	int i;
+
+	if (!print)
+		return -EINVAL;
+
+	vcpu_id = (print == pervcpu) ? &pervcpu->vcpu_id : NULL;
+	if (vcpu_id)
+		prefix = kasprintf(GFP_KERNEL, "Host-vcpu%d: ", *vcpu_id);
+	else
+		prefix = kasprintf(GFP_KERNEL, "Host: ");
+
+	if (!prefix)
+		return -ENOMEM;
+
+	for (summary_stats = (print == pervcpu && summary) ? summary->vmexit_reasons : NULL,
+	     stats = print->vmexit_reasons, i = 0;
+	     i < MAX_EXIT_REASONS;
+	     summary_stats = summary_stats ? summary_stats + 1 : NULL, stats++, i++) {
+		if (!stats->count)
+			continue;
+
+		seq_printf(m, "%s%s %lld cycles %lld each-handler-cycle %lld\n",
+			   prefix, get_vmexit_reason(i), stats->count, stats->cycles,
+			   stats->cycles / stats->count);
+
+		if (summary_stats) {
+			summary_stats->count += stats->count;
+			summary_stats->cycles += stats->cycles;
+		}
+	}
+
+	kfree(prefix);
+
+	return 0;
+}
+
+static int pkvm_dump_vmexit_trace(struct seq_file *m, struct perf_data *dump,
+				  unsigned long size)
+{
+	struct perf_data *summary = kmalloc(sizeof(struct perf_data), GFP_KERNEL_ACCOUNT);
+	struct perf_data *pervcpu;
+	int cpu, ret;
+
+	if (!summary) {
+		pr_err("failed to allocate perf summary buffer\n");
+		return -ENOMEM;
+	}
+
+	for (cpu = 0, pervcpu = dump;
+	     cpu < num_possible_cpus() && size >= sizeof(struct perf_data);
+	     cpu++, size -= sizeof(struct perf_data), pervcpu++) {
+		ret = print_perf_data(m, summary, pervcpu);
+		if (ret)
+			goto out;
+
+		if (need_resched())
+			cond_resched();
+	}
+
+	ret = print_perf_data(m, summary, NULL);
+out:
+	kfree(summary);
+	return ret;
+}
+
+static int vmexit_trace_show(struct seq_file *m, void *unused)
+{
+	struct perf_data *perf;
+	unsigned long size;
+	int ret;
+
+	size = sizeof(struct perf_data) * num_possible_cpus();
+	perf = alloc_pages_exact(size, GFP_KERNEL_ACCOUNT);
+	if (!perf) {
+		pr_err("failed to allocate perf buffer\n");
+		return -ENOMEM;
+	}
+
+	ret = pkvm_hypercall(dump_vmexit_trace, __pa(perf), size);
+	if (ret) {
+		pr_err("failed to get vmexit trace: err %d\n", ret);
+		goto out;
+	}
+
+	ret = pkvm_dump_vmexit_trace(m, perf, size);
+	if (ret)
+		pr_err("failed to dump vmexit trace: err %d\n", ret);
+out:
+	free_pages_exact(perf, size);
+
+	return ret;
+}
+DEFINE_SHOW_ATTRIBUTE(vmexit_trace);
+
 struct debugfs_item {
 	const char *name;
 	const umode_t mode;
@@ -36,6 +156,7 @@ struct debugfs_item {
 
 struct debugfs_item debugfs_files[] = {
 	{ "enable_vmexit_trace", 0222, &enable_vmexit_trace_fops},
+	{ "vmexit_trace", 0444, &vmexit_trace_fops},
 	{ NULL }
 };
 

vineethrp/pKVM-IA

arch/x86/kvm/pkvm_debugfs.c
@@ -4,6 +4,7 @@
 #include <linux/debugfs.h>
 #include <linux/kvm_host.h>
 #include <asm/kvm_pkvm.h>
+#include <asm/pkvm_trace.h>
 
 static void enable_vmexit_trace_func(void *data)
 {
@@ -27,6 +28,124 @@ static int enable_vmexit_trace(void *data, u64 val)
 }
 DEFINE_SIMPLE_ATTRIBUTE(enable_vmexit_trace_fops, NULL, enable_vmexit_trace, "%llu\n");
 
+static const struct vmexit_reason {
+	int reason;
+	char *name;
+} reasons[] = { VMX_EXIT_REASONS, { -1, NULL } };
+
+static const char *get_vmexit_reason(int reason)
+{
+	const struct vmexit_reason *p = reasons;
+
+	while (p->name) {
+		if (p->reason == reason)
+			return p->name;
+		p++;
+	}
+
+	return NULL;
+}
+
+static int print_perf_data(struct seq_file *m, struct perf_data *summary,
+			   struct perf_data *pervcpu)
+{
+	struct perf_data *print = pervcpu ? pervcpu : summary;
+	struct vmexit_stats *stats, *summary_stats;
+	const char *prefix;
+	int *vcpu_id;
+	int i;
+
+	if (!print)
+		return -EINVAL;
+
+	vcpu_id = (print == pervcpu) ? &pervcpu->vcpu_id : NULL;
+	if (vcpu_id)
+		prefix = kasprintf(GFP_KERNEL, "Host-vcpu%d: ", *vcpu_id);
+	else
+		prefix = kasprintf(GFP_KERNEL, "Host: ");
+
+	if (!prefix)
+		return -ENOMEM;
+
+	for (summary_stats = (print == pervcpu && summary) ? summary->vmexit_reasons : NULL,
+	     stats = print->vmexit_reasons, i = 0;
+	     i < MAX_EXIT_REASONS;
+	     summary_stats = summary_stats ? summary_stats + 1 : NULL, stats++, i++) {
+		if (!stats->count)
+			continue;
+
+		seq_printf(m, "%s%s %lld cycles %lld each-handler-cycle %lld\n",
+			   prefix, get_vmexit_reason(i), stats->count, stats->cycles,
+			   stats->cycles / stats->count);
+
+		if (summary_stats) {
+			summary_stats->count += stats->count;
+			summary_stats->cycles += stats->cycles;
+		}
+	}
+
+	kfree(prefix);
+
+	return 0;
+}
+
+static int pkvm_dump_vmexit_trace(struct seq_file *m, struct perf_data *dump,
+				  unsigned long size)
+{
+	struct perf_data *summary = kzalloc(sizeof(struct perf_data), GFP_KERNEL_ACCOUNT);
+	struct perf_data *pervcpu;
+	int cpu, ret;
+
+	if (!summary) {
+		pr_err("failed to allocate perf summary buffer\n");
+		return -ENOMEM;
+	}
+
+	for (cpu = 0, pervcpu = dump;
+	     cpu < num_possible_cpus() && size >= sizeof(struct perf_data);
+	     cpu++, size -= sizeof(struct perf_data), pervcpu++) {
+		ret = print_perf_data(m, summary, pervcpu);
+		if (ret)
+			goto out;
+
+		cond_resched();
+	}
+
+	ret = print_perf_data(m, summary, NULL);
+out:
+	kfree(summary);
+	return ret;
+}
+
+static int vmexit_trace_show(struct seq_file *m, void *unused)
+{
+	struct perf_data *perf;
+	unsigned long size;
+	int ret;
+
+	size = sizeof(struct perf_data) * num_possible_cpus();
+	perf = alloc_pages_exact(size, GFP_KERNEL_ACCOUNT);
+	if (!perf) {
+		pr_err("failed to allocate perf buffer\n");
+		return -ENOMEM;
+	}
+
+	ret = pkvm_hypercall(dump_vmexit_trace, __pa(perf), size);
+	if (ret) {
+		pr_err("failed to get vmexit trace: err %d\n", ret);
+		goto out;
+	}
+
+	ret = pkvm_dump_vmexit_trace(m, perf, size);
+	if (ret)
+		pr_err("failed to dump vmexit trace: err %d\n", ret);
+out:
+	free_pages_exact(perf, size);
+
+	return ret;
+}
+DEFINE_SHOW_ATTRIBUTE(vmexit_trace);
+
 struct debugfs_item {
 	const char *name;
 	const umode_t mode;
@@ -36,6 +155,7 @@ struct debugfs_item {
 
 struct debugfs_item debugfs_files[] = {
 	{ "enable_vmexit_trace", 0222, &enable_vmexit_trace_fops},
+	{ "vmexit_trace", 0444, &vmexit_trace_fops},
 	{ NULL }
 };
 