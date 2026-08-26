5b6b6f85c894921b258745a42eaaa00e32ae8c81
Vineeth Pillai <vineethrp@google.com>
2026-08-26 06:37:38 -0400

    ANDROID: iommu/vt-d: pKVM: Reject S3 suspend and resume
    pKVM does not support S3 suspend. Reject the IOMMU suspend callback
    when pKVM is enabled and warn if either the suspend or resume callback is
    reached.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>
    
    Bug: None
    Change-Id: I4a51871f591e725fcc466e72944671f2aa899cf9

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -2112,6 +2112,10 @@ static int iommu_suspend(void *data)
 	struct intel_iommu *iommu = NULL;
 	unsigned long flag;
 
+	/* pKVM doesn't support S3 suspend */
+	if (WARN_ON(pkvm_enabled()))
+		return -EOPNOTSUPP;
+
 	iommu_flush_all();
 
 	for_each_active_iommu(iommu, drhd) {
@@ -2138,6 +2142,10 @@ static void iommu_resume(void *data)
 	struct dmar_drhd_unit *drhd;
 	struct intel_iommu *iommu = NULL;
 	unsigned long flag;
+
+	/* pKVM doesn't support S3 suspend */
+	if (WARN_ON(pkvm_enabled()))
+		return;
 
 	if (init_iommu_hw()) {
 		if (force_on)
