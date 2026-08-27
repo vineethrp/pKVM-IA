2c768c2118d3635228eb06de63f485f126fd8332
Vineeth Pillai <vineethrp@google.com>
2026-08-26 20:02:51 -0400

    ANDROID: pKVM: VMX: Expose the protected host EPT geometry
    Expose the root and level of the protected host EPT to other hypervisor
    subsystems.
    
    Protected IOMMU passthrough entries use the host EPT rather than unrestricted
    VT-d passthrough so DMA remains confined to host-owned memory. The domain
    framework also uses this geometry to initialize its permanent passthrough
    domain.
    
    Keep the accessors local to the protected hypervisor and reject calls before
    the host EPT has been initialized.
    
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>
    
    Bug: None
    Change-Id: I2fce6a79b2ccbd85df4ad7874ed32ee3c7d961b7

--- arch/x86/kvm/pkvm/vmx/ept.h
+++ arch/x86/kvm/pkvm/vmx/ept.h
@@ -7,6 +7,8 @@
 int pkvm_host_ept_init(struct pkvm_pgtable *pgt, void *pool_base,
 		       unsigned long pool_pages);
 int pkvm_host_ept_finalize(struct pkvm_pgtable *pgt);
+phys_addr_t pkvm_host_ept_root(void);
+int pkvm_host_ept_level(void);
 void pkvm_handle_host_ept_violation(struct kvm_vcpu *vcpu);
 void pkvm_flush_host_ept(void);
 
