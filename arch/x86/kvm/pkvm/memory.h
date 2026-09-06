cxdong/pub-pKVM-IA

pKVM: x86: Add helper to flush cpu cache
Add helper pkvm_clflush_cache_range to flush cpu cache for a specific
memory range. This can be used before donating a memory page from the
pKVM hypervisor to the host, to make sure the host cannot get the
previous contents in RAM via the non-coherent DMA.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/memory.h
@@ -91,4 +91,6 @@ static inline bool is_memory_range(unsigned long phys, unsigned long size)
 	return range_contains(&range, &target);
 }
 
+void pkvm_clflush_cache_range(void *vaddr, unsigned int size);
+
 #endif /* __PKVM_X86_MEMORY_H */

vineethrp/pKVM-IA

arch/x86/kvm/pkvm/memory.h
@@ -97,4 +97,6 @@ static inline bool is_memory_range(unsigned long phys, unsigned long size)
 	return range_contains(&range, &target);
 }
 
+void clflush_cache_range(void *vaddr, unsigned int size);
+
 #endif /* __PKVM_X86_MEMORY_H */