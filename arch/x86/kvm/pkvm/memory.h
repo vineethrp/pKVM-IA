cxdong/pub-pKVM-IA

pKVM: x86: Clear memory pages before donate to host
To make sure no sensitive data is leaked to the host after donating
the memory pages used by the pKVM hypervisor, clear the contents in
such memory pages if it is necessary. Also flush the cpu cache to
prevent the host from getting the previous contents in RAM via
non-coherent DMA.

Suggested-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/memory.h
@@ -99,4 +99,14 @@ static inline bool is_memory_range(unsigned long phys, unsigned long size)
 
 void pkvm_clflush_cache_range(void *vaddr, unsigned int size);
 
+static inline void pkvm_clear_memory(void *va, size_t size)
+{
+	memset(va, 0, size);
+	/*
+	 * Flush CPU cache to ensure clearing the memory range in RAM, so that
+	 * the previous contents cannot be read via non-coherent DMA.
+	 */
+	pkvm_clflush_cache_range(va, size);
+}
+
 #endif /* __PKVM_X86_MEMORY_H */

vineethrp/pKVM-IA

arch/x86/kvm/pkvm/memory.h
@@ -99,4 +99,14 @@ static inline bool is_memory_range(unsigned long phys, unsigned long size)
 
 void clflush_cache_range(void *vaddr, unsigned int size);
 
+static inline void pkvm_clear_memory(void *va, size_t size)
+{
+	memset(va, 0, size);
+	/*
+	 * Flush CPU cache to ensure clearing the memory range in RAM, so that
+	 * the previous contents cannot be read via non-coherent DMA.
+	 */
+	clflush_cache_range(va, size);
+}
+
 #endif /* __PKVM_X86_MEMORY_H */